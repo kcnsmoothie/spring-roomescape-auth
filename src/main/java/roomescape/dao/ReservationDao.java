fbcb37f2cd762019936a31668042c4449e806124
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-05-21 16:29:34 +0900

    feat: 전체 예약 조회 기능 구현

--- src/main/java/roomescape/dao/ReservationDao.java
+++ src/main/java/roomescape/dao/ReservationDao.java
@@ -123,6 +123,40 @@ public class ReservationDao {
         ).stream().findFirst();
     }
 
+    public List<Reservation> findAllByName(String name) {
+        return jdbcTemplate.query(
+                """
+                            SELECT r.id, r.name, r.date, rt.id AS time_id, rt.start_at,
+                            t.id AS theme_id, t.name AS theme_name, t.description, t.url
+                            FROM reservation r
+                            INNER JOIN reservation_time rt ON r.time_id = rt.id
+                            INNER JOIN theme t ON r.theme_id = t.id
+                            WHERE r.name = ?
+                            ORDER BY r.date, rt.start_at
+                        """,
+                (rs, rowNum) -> {
+                    ReservationTime time = new ReservationTime(
+                            rs.getLong("time_id"),
+                            rs.getTime("start_at").toLocalTime()
+                    );
+                    Theme theme = new Theme(
+                            rs.getLong("theme_id"),
+                            rs.getString("theme_name"),
+                            rs.getString("description"),
+                            rs.getString("url")
+                    );
+                    return new Reservation(
+                            rs.getLong("id"),
+                            rs.getString("name"),
+                            rs.getDate("date").toLocalDate(),
+                            time,
+                            theme
+                    );
+                },
+                name
+        );
+    }
+
     public boolean existsBy(LocalDate date, Theme theme, ReservationTime time) {
         Boolean result = jdbcTemplate.queryForObject("""
         SELECT EXISTS(
