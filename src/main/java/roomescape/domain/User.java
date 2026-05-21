686729c98256e79a48c946090299a2f25cc3c8bc
kcnsmoothie <kcnsmoothie05@gmail.com>
2026-05-21 13:23:57 +0900

    feat: 사용자 도메인 추가

---
+++ src/main/java/roomescape/domain/User.java
@@ -0,0 +1,13 @@
+package roomescape.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+}
